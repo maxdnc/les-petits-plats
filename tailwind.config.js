3412d13ff97a6db71a6c2b72fa92b49e2766f8f2
Maxime Danic <maxime.danic@gmail.com>
2024-03-25 20:52:13 +0200

    Add font in index and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './index.html'],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackPrimary: "#1B1B1B",
+        darkGray: "#7A7A7A",
+        yellowSecondary: "#FFD15B",
+        lightGray: "#EDEDED",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        anton: ["Anton", "sans-serif"],
+      },
+    },
   },
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
