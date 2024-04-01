cb957b6479f665dfc546fc0fe0981105016ac2a3
Maxime Danic <maxime.danic@gmail.com>
2024-04-01 13:29:53 +0300

    Add ListCards component and render recipes list dynamic

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,12 @@
 import Dropdown from "./components/ClassDropDown.js";
+import ListCards from "./components/ListCards.js";
+import recipes from "../api/recipes.js";
+
+const listRecipesSection = document.querySelector("#ListRecipes");
+const recipesList = recipes;
+console.log(recipesList);
+
+listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
 const myDropdown = new Dropdown(
   "dropdown-button",
