befb40ae8180f244ec4f65e40c5d3f32b729447e
Maxime Danic <maxime.danic@gmail.com>
2024-04-01 22:43:11 +0300

    Refactor dropdown imports and add filter dropdowns

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,17 +1,28 @@
-import Dropdown from "./components/ClassDropDown.js";
+import DropdownItem from "./components/dropDown.js";
 import ListCards from "./components/ListCards.js";
 import recipes from "../api/recipes.js";
+import getUniqueValues from "./utils/getUniqueValues.js";
 
 const listRecipesSection = document.querySelector("#ListRecipes");
+const filtersMenu = document.querySelector("#filtersMenu");
+const numberOfRecipes = document.querySelector("#numberOfRecipes");
+
 const recipesList = recipes;
-console.log(recipesList);
+numberOfRecipes.textContent = `${recipesList.length} ${recipesList.length <= 1 ? "recette" : "recettes"}`;
 
 listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
-const myDropdown = new Dropdown(
-  "dropdown-button",
-  "dropdown-menu",
-  "search-input",
-  "arrow-DropDown",
-  "delete-input",
-);
+const allUstensils = getUniqueValues(recipesList, "ustensils");
+const allappliance = getUniqueValues(recipesList, "appliance");
+const allIngredients = getUniqueValues(recipesList, "ingredients");
+
+const dropdownIngredient = new DropdownItem("Ingrédients", allIngredients);
+const dropdownAppliance = new DropdownItem("Appareils", allappliance);
+const dropdownUstensils = new DropdownItem("Ustensiles", allUstensils);
+
+const dropdownElement = dropdownIngredient.createDropdown();
+const dropdownElement2 = dropdownAppliance.createDropdown();
+const dropdownElement3 = dropdownUstensils.createDropdown();
+filtersMenu.appendChild(dropdownElement);
+filtersMenu.appendChild(dropdownElement2);
+filtersMenu.appendChild(dropdownElement3);
