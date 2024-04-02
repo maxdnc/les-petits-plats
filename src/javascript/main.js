c745a2edb148c0f05342f5854ca35cef3c6a9ba8
Maxime Danic <maxime.danic@gmail.com>
2024-04-02 22:54:37 +0300

    Add utils function createAndAppendDropdown to main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,6 +3,7 @@ import ListCards from "./components/ListCards.js";
 import recipes from "../api/recipes.js";
 import getUniqueValues from "./utils/getUniqueValues.js";
 import searchAlgorithm from "./services/searchAlgorithm.js";
+import createAndAppendDropdown from "./utils/createAndAppendDropdown.js";
 
 const listRecipesSection = document.querySelector("#ListRecipes");
 const filtersMenu = document.querySelector("#filtersMenu");
@@ -13,20 +14,9 @@ numberOfRecipes.textContent = `${recipesList.length} ${recipesList.length <= 1 ?
 
 listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
-const allUstensils = getUniqueValues(recipesList, "ustensils");
-const allappliance = getUniqueValues(recipesList, "appliance");
-const allIngredients = getUniqueValues(recipesList, "ingredients");
-
-const dropdownIngredient = new DropdownItem("Ingrédients", allIngredients);
-const dropdownAppliance = new DropdownItem("Appareils", allappliance);
-const dropdownUstensils = new DropdownItem("Ustensiles", allUstensils);
-
-const dropdownElement = dropdownIngredient.createDropdown();
-const dropdownElement2 = dropdownAppliance.createDropdown();
-const dropdownElement3 = dropdownUstensils.createDropdown();
-filtersMenu.appendChild(dropdownElement);
-filtersMenu.appendChild(dropdownElement2);
-filtersMenu.appendChild(dropdownElement3);
+createAndAppendDropdown(recipesList, "ustensils", "Ustensiles", filtersMenu);
+createAndAppendDropdown(recipesList, "appliance", "Appareils", filtersMenu);
+createAndAppendDropdown(recipesList, "ingredients", "Ingrédients", filtersMenu);
 
 const searchForm = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
