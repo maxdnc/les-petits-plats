51d102231fc9808df6f86e745df4d4cdcaf12456
Maxime Danic <maxime.danic@gmail.com>
2024-04-03 21:35:26 +0300

    Add dropdown for ingredients in filters menu

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -14,9 +14,9 @@ numberOfRecipes.textContent = `${recipesList.length} ${recipesList.length <= 1 ?
 
 listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
+createAndAppendDropdown(recipesList, "ingredients", "Ingrédients", filtersMenu);
 createAndAppendDropdown(recipesList, "ustensils", "Ustensiles", filtersMenu);
 createAndAppendDropdown(recipesList, "appliance", "Appareils", filtersMenu);
-createAndAppendDropdown(recipesList, "ingredients", "Ingrédients", filtersMenu);
 
 const searchForm = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
