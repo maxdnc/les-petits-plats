a83a59d8579757cb22b705851efbf6b37f432c48
Maxime Danic <maxime.danic@gmail.com>
2024-04-04 19:46:24 +0300

    Add form event listener to perform search and update results

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -9,19 +9,15 @@ import createAndAppendDropdown from "./utils/createAndAppendDropdown.js";
 const listRecipesSection = document.querySelector("#ListRecipes");
 const filtersMenu = document.querySelector("#filtersMenu");
 const numberOfRecipes = document.querySelector("#numberOfRecipes");
+const form = document.querySelector("#searchForm");
 
-const recipesList = recipes;
+let recipesList = recipes;
 numberOfRecipes.textContent = `${recipesList.length} ${recipesList.length <= 1 ? "recette" : "recettes"}`;
 
 listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
 const searchForm = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
-
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  searchAlgorithm(searchInput.value);
-});
 
 let selectedItems = [];
 
@@ -57,3 +53,10 @@ createAndAppendDropdown(
   onItemDeselected,
   selectedItems,
 );
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const results = searchAlgorithm(searchInput.value, recipesList);
+  listRecipesSection.innerHTML = new ListCards(results).render();
+  numberOfRecipes.textContent = `${results.length} ${results.length <= 1 ? "recette" : "recettes"}`;
+});
