2bb4f91aa496e9b51bee0888a35a524290d7247e
Maxime Danic <maxime.danic@gmail.com>
2024-04-04 22:17:53 +0300

    Add tagAlgorithm.js with recipe search by tag algorithm

---
+++ src/javascript/services/tagAlgorithm.js
@@ -0,0 +1,27 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable max-len */
+/* eslint-disable operator-linebreak */
+export default function rechercherRecettesParTag(recipes, tags) {
+  if (tags.length === 0) {
+    return recipes;
+  }
+
+  return recipes.filter((recipe) => {
+    // Vérifier si au moins un des tags est présent dans les ingrédients, ustensiles ou appareil de la recette
+    return tags.some((tag) => {
+      // Vérifier si le tag est présent dans les ingrédients
+      const ingredientPresent = recipe.ingredients.some(
+        (ingredient) =>
+          ingredient.ingredient.toLowerCase() === tag.toLowerCase(),
+      );
+      // Vérifier si le tag est présent dans les ustensiles
+      const ustensilPresent = recipe.ustensils.some(
+        (ustensil) => ustensil.toLowerCase() === tag.toLowerCase(),
+      );
+      // Vérifier si le tag est présent dans l'appareil
+      const appliancePresent =
+        recipe.appliance.toLowerCase() === tag.toLowerCase();
+      return ingredientPresent || ustensilPresent || appliancePresent;
+    });
+  });
+}
