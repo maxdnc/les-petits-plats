b31171d5199b4fb54f523e4229eee01dd2f66fbe
Maxime Danic <maxime.danic@gmail.com>
2024-04-04 19:46:30 +0300

    Refactor searchAlgorithm function to include search by ingredients

--- src/javascript/services/searchAlgorithm.js
+++ src/javascript/services/searchAlgorithm.js
@@ -1,3 +1,24 @@
-export default function searchAlgorithm(searchValue) {
-  console.log(searchValue);
+/* eslint-disable operator-linebreak */
+export default function searchAlgorithm(searchValue, recipes) {
+  const results = [];
+
+  for (let i = 0; i < recipes.length; i += 1) {
+    const recipe = recipes[i];
+    let ingredients = "";
+
+    for (let j = 0; j < recipe.ingredients.length; j += 1) {
+      ingredients += `${recipe.ingredients[j].ingredient} `;
+    }
+    ingredients = ingredients.trim();
+
+    if (
+      recipe.name.includes(searchValue) ||
+      recipe.description.includes(searchValue) ||
+      ingredients.includes(searchValue)
+    ) {
+      results.push(recipe);
+    }
+  }
+
+  return results;
 }
