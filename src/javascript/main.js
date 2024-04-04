eeac6d16abc30521d8fd37f85f6dc28c357bbed6
Maxime Danic <maxime.danic@gmail.com>
2024-04-04 03:10:21 +0300

    Add dropdown functionality to filter menu

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import DropdownItem from "./components/dropDown.js";
 import ListCards from "./components/ListCards.js";
 import recipes from "../api/recipes.js";
@@ -14,10 +15,6 @@ numberOfRecipes.textContent = `${recipesList.length} ${recipesList.length <= 1 ?
 
 listRecipesSection.innerHTML = new ListCards(recipesList).render();
 
-createAndAppendDropdown(recipesList, "ingredients", "Ingrédients", filtersMenu);
-createAndAppendDropdown(recipesList, "ustensils", "Ustensiles", filtersMenu);
-createAndAppendDropdown(recipesList, "appliance", "Appareils", filtersMenu);
-
 const searchForm = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
 
@@ -25,3 +22,38 @@ searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
   searchAlgorithm(searchInput.value);
 });
+
+let selectedItems = [];
+
+const onItemDeselected = (item) => {
+  const index = selectedItems.indexOf(item);
+  if (index !== -1) {
+    selectedItems.splice(index, 1);
+  }
+  console.log(selectedItems);
+};
+
+createAndAppendDropdown(
+  recipesList,
+  "ingredients",
+  "Ingrédients",
+  filtersMenu,
+  onItemDeselected,
+  selectedItems,
+);
+createAndAppendDropdown(
+  recipesList,
+  "ustensils",
+  "Ustensiles",
+  filtersMenu,
+  onItemDeselected,
+  selectedItems,
+);
+createAndAppendDropdown(
+  recipesList,
+  "appliance",
+  "Appareils",
+  filtersMenu,
+  onItemDeselected,
+  selectedItems,
+);
