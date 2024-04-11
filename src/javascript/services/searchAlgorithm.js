e24713d1f1e1f2ca53364439c3ccde279288475a
Maxime Danic <maxime.danic@gmail.com>
2024-04-11 21:49:35 +0300

    Refactor searchAlgorithm function to improve search functionality and optimize ingredient filtering

--- src/javascript/services/searchAlgorithm.js
+++ src/javascript/services/searchAlgorithm.js
@@ -1,25 +1,16 @@
 /* eslint-disable operator-linebreak */
 export default function searchAlgorithm(searchValue, recipes) {
-  const results = [];
   const lowerCaseSearchValue = searchValue.toLowerCase();
 
-  for (let i = 0; i < recipes.length; i += 1) {
-    const recipe = recipes[i];
-    let ingredients = "";
+  return recipes.filter((recipe) => {
+    const ingredients = recipe.ingredients
+      .map((ingredient) => ingredient.ingredient.toLowerCase())
+      .join(" ");
 
-    for (let j = 0; j < recipe.ingredients.length; j += 1) {
-      ingredients += `${recipe.ingredients[j].ingredient} `;
-    }
-    ingredients = ingredients.trim().toLowerCase();
-
-    if (
+    return (
       recipe.name.toLowerCase().includes(lowerCaseSearchValue) ||
       recipe.description.toLowerCase().includes(lowerCaseSearchValue) ||
       ingredients.includes(lowerCaseSearchValue)
-    ) {
-      results.push(recipe);
-    }
-  }
-
-  return results;
+    );
+  });
 }
